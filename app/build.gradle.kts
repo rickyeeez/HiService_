8712c324808f1c618a6f9049e670052d1abf64fe
rickyeeez <wardhanaricky30@gmail.com>
2023-11-20 14:40:52 +0700

    Added Boarding Picture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.hiservice.mobile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hiservice.mobile"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
