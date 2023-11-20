5f4289e8bd4c2f8e3af23b90a326a33ac9a0831b
rickyeeez <wardhanaricky30@gmail.com>
2023-11-20 14:50:37 +0700

    Added Boarding Picture

--- app/src/main/java/com/hiservice/mobile/MainActivity.kt
+++ app/src/main/java/com/hiservice/mobile/MainActivity.kt
@@ -3,12 +3,17 @@ package com.hiservice.mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import com.hiservice.mobile.ui.theme.HiServiceTheme
 
@@ -35,11 +40,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    Column {
+        Text("Hello World", fontFamily = FontFamily.Serif)
+        Text("Hello World", fontFamily = FontFamily.SansSerif)
+    }
+    Image(
+        painter = painterResource(id = R.drawable.on_board_1),
+        contentDescription = stringResource(id = R.string.onBoardDesc)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+
     HiServiceTheme {
         Greeting("Android")
     }
